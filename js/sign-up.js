86be3c3a4383097836c2c708c4214e88e4c2228c
Justin <jmdinkelbach@gmail.com>
2020-11-14 23:04:23 +0000

    added form validations

--- js/sign-up.js
+++ js/sign-up.js
@@ -6,19 +6,26 @@ var usernameAvailable = false;
 // Get city, latitude, longitude from API after entering zip
 $("#zip").on("change", async function(){
     let zipCode = $("#zip").val();
-    let url =  `https://cst336.herokuapp.com/projects/api/cityInfoAPI.php?zip=${zipCode}`;
+    let url =  `https://itcdland.csumb.edu/~milara/ajax/cityInfoByZip.php?zip=${zipCode}`;
+    console.log("fetching");
     let response = await fetch(url);
+    console.log("done fetching");
     let data = await response.json();
     // Fill form with retrieved data
-    $("#city").html(data.city);
-    $("#latitude").html(data.latitude);
-    $("#longitude").html(data.longitude);
+    if(data){
+        $("#city").html(data.city);
+        $("#latitude").html(data.latitude);
+        $("#longitude").html(data.longitude);
+    }
+    else{
+        $("#zipError").html("Zip code not found.");
+    }
 })
 
 // Populate list of counties based on selected state
 $("#state").on("change", async function(){
     let state = $("#state").val();
-    let url = `https://cst336.herokuapp.com/projects/api/countyListAPI.php?state=${state}`;
+    let url = `https://itcdland.csumb.edu/~milara/ajax/countyList.php?state=${state}`;
     let response = await fetch(url);
     let data = await response.json();
     for (let i = 0; i < data.length; i++){
@@ -46,28 +53,31 @@ $("#username").on("change", async function(){
 })
 
 $("#signupForm").on("submit", function(e){
+    console.log("submit clicked");
     if(!isFormValid()){
         e.preventDefault();  
     }
-});
+})
 
 function isFormValid(){
     var isValid = true;
+    console.log($("#password").val());
     
     if (!usernameAvailable){
-        isValid = false;
+        isValid = false
     }
     if ($("#username").val().length === 0){
         isValid = false;
-        $("#username").html("Username is required.");
+        $("#usernameError").html("Username is required.");
     }
     if ($("#password").val() != $("#passwordAgain").val()){
-        $("#passwordAgainError").html("Passwords must match.");
+        $("#passwordAgainError").html("Retype password.");
         isValid = false;
     }
     if ($("#password").val().length < 6){
         $("#passwordAgainError").html("Passwords must be at least 6 characters.");
         isValid = false;
     }
+    console.log(isValid);
     return isValid;
 }
